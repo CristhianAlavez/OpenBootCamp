ccbdd51587881035eb004794f4be83abf2a44675
Cristhian Eder Alavez Barrita <calavez@comunidad.unam.mx>
2022-03-31 11:38:33 -0600

    Add files via upload

---
+++ Introduccion a la programación/Ejercicios tema 4/Case.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        var estacion = "INVIERNO";
+
+        switch (estacion) {
+            case "PRIMAVERA":
+                System.out.println("es primavera");
+            case "VERANO":
+                System.out.println("es primavera");
+            case "OTOÑO":
+                System.out.println("es primavera");
+            case "INVIERNO":
+                System.out.println("es primavera");
+                break;
+            default:
+                System.out.println("no es una estación");
+        }
+    }
+}
---
+++ Introduccion a la programación/Ejercicios tema 4/If.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int numeroIf = 0;
+
+        if (numeroIf < 0) {
+            System.out.println ("El dato es negativo");
+        } else if (numeroIf > 0) {
+            System.out.println ("El dato es positivo");
+        } else {
+            System.out.println ("El dato es igual a cero");
+        }
+    }
+
+}
